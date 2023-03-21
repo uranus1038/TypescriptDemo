718d646b04b5c8d73efbda8f3e241c4357f1b825
you <anusorn1039@gmail.com>
2023-03-22 01:45:41 +0700

    commit del

--- YUMIServices/config/database.ts
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql2/promise'); 
-const {host , user , password , database} = require('../');
-const UMIQuery = mysql.createPool({
-    host: host,
-    user: user,
-    password: password,
-    database: database
-  });
-  module.exports = UMIQuery;
--- YUMIServices/services/app.ts
+++
@@ -1,7 +0,0 @@
-import {App} from "../UMIFramwork/app";
-
-const server = new App();
-server.listen(8000) ; 
-server.getPath("/gg" , "hello world" , (data)=>{
-    console.log(data) ; 
-});
