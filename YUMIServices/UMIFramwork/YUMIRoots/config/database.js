4a6566dda1023e534fc0cfacf7583739310437f9
you <anusorn1039@gmail.com>
2023-03-22 01:39:00 +0700

    Add set mysql

---
+++ YUMIServices/UMIFramwork/YUMIRoots/config/database.js
@@ -0,0 +1,10 @@
+"use strict";
+const mysql = require('mysql2/promise');
+const { host, user, password, database } = require('../');
+const UMIQuery = mysql.createPool({
+    host: host,
+    user: user,
+    password: password,
+    database: database
+});
+module.exports = UMIQuery;
